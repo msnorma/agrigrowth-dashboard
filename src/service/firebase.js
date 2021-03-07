510020ae8be09bbbbad61136b31b6b14983e3bd0
Tiisetso Modubu <tiisetsomodubu@Tiisetsos-MBP.broadband>
2021-03-07 19:42:15 +0000

    fix: googl auth

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -6,13 +6,13 @@ dotenv.config();
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBwazMOv2MyiI6rlG6Lq34RJJ82PXilw2U",
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID
+	authDomain: "agrigrowth-7899e.firebaseapp.com",
+	databaseURL: "https://agrigrowth-7899e-default-rtdb.firebaseio.com",
+	projectId: "agrigrowth-7899e",
+	storageBucket: "agrigrowth-7899e.appspot.com",
+	messagingSenderId: "363674113106",
+	appId: "1:363674113106:web:a00bd7cd706d553f5a7b8b",
+	measurementId: "G-V0Y3BLTYX7"
 };
 // firebase.analytics();
 
